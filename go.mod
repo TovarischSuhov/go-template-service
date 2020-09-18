d16e52bcd05d248b66c1a6eb4f257f2a2649c3d7
Ilya Sukhov <i.suhov@city-mobil.ru>
2020-09-18 11:32:24 +0300

    Init http service

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mod
 
 go 1.13
+
+require github.com/gorilla/mux v1.8.0
--- src/go-microservice-template/main/main.go
+++ src/go-microservice-template/main/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "localhost:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+func ArticlesHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "article")
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "home")
+}
+
+func ProductsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "product")
 }
